bdefb619d4c9061727c9d122ce6cf4b930a9c499
Ivan Homziak <ivan.homziak@gmail.com>
2021-06-04 20:32:02 +0300

    04-06-21/20:21

---
+++ driver.java
@@ -0,0 +1,11 @@
+package basicTest;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class driver {
+
+    static WebDriver driver = new FirefoxDriver();
+
+}
